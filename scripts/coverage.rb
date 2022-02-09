d3c4632c4de740a9f1f42dde4e4342694db16454
Derek Bronston <dbronston@me.com>
2022-02-09 16:43:51 -0500

    updated

--- scripts/coverage.rb
+++ scripts/coverage.rb
@@ -1,3 +1,6 @@
 #!/usr/bin/env ruby
 
-puts("test put")
+SCHEME = "TestMe"
+RESULT_BUNDLE = "CodeCoverage.xcresult"
+RESULT_JSON = "CodeCoverage.json"
+MIN_CODE_COVERAGE = 50.0 # should really be higher =)
