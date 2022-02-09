bd2976f82867c9654e9e5f437a4876015ada9250
Derek Bronston <dbronston@me.com>
2022-02-09 09:31:14 -0500

    init

---
+++ TestMe/TestClass.swift
@@ -0,0 +1,20 @@
+//
+//  TestClass.swift
+//  TestMe
+//
+//  Created by Derek Bronston on 2/9/22.
+//
+
+import Foundation
+
+
+class TestClass {
+    
+    func returnInt() -> Int {
+        return 1
+    }
+    
+    func returnString() -> String {
+        return "hello world"
+    }
+}
