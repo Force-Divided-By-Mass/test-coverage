64ad650aa8c6377f9591b8e226f5bdbb8592cc74
Derek Bronston <dbronston@me.com>
2022-02-12 08:30:17 -0500

    updated

--- scripts/coverage.rb
+++ scripts/coverage.rb
@@ -24,7 +24,7 @@ end
 @project.coverage_service = :terminal  
 @project.configure
 coverage = total_coverage(@project)
-puts('Total Coverage: ' + coverage + '%')
+puts('Total Coverage: ' + coverage.to_s + '%')
 if coverage < 90 
 	puts('Erro: Low Coverage')
 	exit -1
